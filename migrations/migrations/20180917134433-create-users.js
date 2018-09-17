3ce3e2a33909d19613ce832d21e42ad0765fab2f
bhflm <germanbonin@gmail.com>
2018-09-17 13:59:32 -0300

    Fixed previous commit, added the user model for the db

--- migrations/migrations/20180917134433-create-users.js
+++ migrations/migrations/20180917134433-create-users.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Usernames', {
+    return queryInterface.createTable('usernames', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Usernames');
+    return queryInterface.dropTable('usernames');
   }
 };
