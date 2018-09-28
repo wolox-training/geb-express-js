0b16ac0ed6726a9be91c00b8ac7c561aba9407b5
bhflm <germanbonin@gmail.com>
2018-09-28 12:15:09 -0300

    fixed the branch with the previous pr and some typos

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -6,7 +6,6 @@ const users = require('../models').users,
   VALID_CL = /^\w+([\.-]?\w+)*@\wolox.cl/;
 
 exports.validateEmail = email => {
-<<<<<<< HEAD
   if (!VALID_AR.test(email) && !VALID_CO.test(email) && !VALID_CL.test(email)) {
     return errors.INVALID_EMAIL;
   }
@@ -16,17 +15,4 @@ exports.validatePassword = password => {
   if (!VALID_ALPHANUM.test(password) || password.length < 8) {
     return errors.INVALID_PASSWORD;
   }
-=======
-  if (VALID_AR.test(email) || VALID_CO.test(email) || VALID_CL.test(email)) {
-    return true;
-  }
-  throw errors.invalidEmail();
-};
-
-exports.validatePassword = pwd => {
-  if (VALID_ALPHANUM.test(pwd) && pwd.length >= 8) {
-    return true;
-  }
-  throw errors.invalidPassword();
->>>>>>> e883197e8f4e0a354551e3c22c54d7d0660c411f
 };
