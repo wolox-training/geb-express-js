0694075b7b833f9e4a2956e38e6f2e7d7f95c5d7
bhflm <germanbonin@gmail.com>
2018-10-02 15:33:55 -0300

    fix typo

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,21 +6,24 @@ const users = require('../models').users,
   sessionManager = require('../services/sessionManager');
 
 exports.logIn = (req, res, next) => {
-  return users.findUser(req.body.email).then(u => {
-    if (u) {
-      return bcrypt.compare(req.body.password, u.password).then(valid => {
-        if (valid) {
-          const token = sessionManager.encode({ user: u.username });
-          res.set(sessionManager.HEADER, token);
-          res.status(200);
-          res.end();
-        } else next(errors.invalidPassword());
-      });
-    } else next(errors.invalidUsername());
-  }).catch(err =>{
-    logger.info('DB Error');
-    next(err);
-  });
+  return users
+    .findUser(req.body.email)
+    .then(u => {
+      if (u) {
+        return bcrypt.compare(req.body.password, u.password).then(valid => {
+          if (valid) {
+            const token = sessionManager.encode({ user: u.username });
+            res.set(sessionManager.HEADER, token);
+            res.status(200);
+            res.end();
+          } else next(errors.invalidPassword());
+        });
+      } else next(errors.invalidUsername());
+    })
+    .catch(err => {
+      logger.info('DB Error');
+      next(err);
+    });
 };
 
 exports.signUp = (req, res, next) => {
