3fc1c3ce533a0809b946589f5e6d447a6411c399
bhflm <germanbonin@gmail.com>
2018-10-09 17:34:02 -0300

    modified albums service, users now see reduced info from the fetch

--- app/services/albumsManager.js
+++ app/services/albumsManager.js
@@ -1,5 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.listAlbums = () => {
-  return fetch('https://jsonplaceholder.typicode.com/albums').then(response => response.json());
+  return fetch('https://jsonplaceholder.typicode.com/albums').then(response =>
+    response.json().then(albums => {
+      const titles = [];
+      albums.forEach(album => {
+        titles.push(album.title);
+      });
+      return titles;
+    })
+  );
 };
