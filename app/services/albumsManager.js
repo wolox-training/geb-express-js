973189e64c7e3fdad703654d82c00ac056a4e0df
bhflm <germanbonin@gmail.com>
2018-10-09 16:44:58 -0300

    implemented list all albums with tests

--- app/services/albumsManager.js
+++ app/services/albumsManager.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.listAlbums = () => {
+<<<<<<< 3c59db3552377286f261a4ca9cc2f0b9572f729a
   return fetch('https://jsonplaceholder.typicode.com/albums').then(response =>
     response.json().then(albums => {
       const titles = [];
@@ -10,4 +11,7 @@ exports.listAlbums = () => {
       return titles;
     })
   );
+=======
+  return fetch('https://jsonplaceholder.typicode.com/albums').then(response => response.json());
+>>>>>>> implemented list all albums with tests
 };
