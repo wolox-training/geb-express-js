087cf15d3f2b24ce9ba4be5467335459b10d4497
bhflm <germanbonin@gmail.com>
2018-10-09 17:34:02 -0300

    modified albums service, users now see reduced info from the fetch

--- app/services/albumsManager.js
+++ app/services/albumsManager.js
@@ -1,17 +1,27 @@
 const fetch = require('node-fetch');
 
 exports.listAlbums = () => {
+<<<<<<< d02b817d7e4dcbcb769aa0655ce3e9b1a4d0cdcd
 <<<<<<< 3c59db3552377286f261a4ca9cc2f0b9572f729a
+=======
+>>>>>>> modified albums service, users now see reduced info from the fetch
   return fetch('https://jsonplaceholder.typicode.com/albums').then(response =>
     response.json().then(albums => {
       const titles = [];
       albums.forEach(album => {
+<<<<<<< d02b817d7e4dcbcb769aa0655ce3e9b1a4d0cdcd
         titles.push({ id: album.id, album: album.title });
+=======
+        titles.push(album.title);
+>>>>>>> modified albums service, users now see reduced info from the fetch
       });
       return titles;
     })
   );
+<<<<<<< d02b817d7e4dcbcb769aa0655ce3e9b1a4d0cdcd
 =======
   return fetch('https://jsonplaceholder.typicode.com/albums').then(response => response.json());
 >>>>>>> implemented list all albums with tests
+=======
+>>>>>>> modified albums service, users now see reduced info from the fetch
 };
