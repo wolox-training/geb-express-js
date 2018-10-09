b1df0b51fda453673aeb3620b69347b8290285fc
bhflm <germanbonin@gmail.com>
2018-10-09 19:08:59 -0300

    minor changes

--- app/services/albumsManager.js
+++ app/services/albumsManager.js
@@ -9,11 +9,15 @@ exports.listAlbums = () => {
     response.json().then(albums => {
       const titles = [];
       albums.forEach(album => {
+<<<<<<< 5a28acaa0d9c6218e1be4a6b566b02d4954507c0
 <<<<<<< d02b817d7e4dcbcb769aa0655ce3e9b1a4d0cdcd
         titles.push({ id: album.id, album: album.title });
 =======
         titles.push(album.title);
 >>>>>>> modified albums service, users now see reduced info from the fetch
+=======
+        titles.push({ id: album.id, album: album.title });
+>>>>>>> minor changes
       });
       return titles;
     })
