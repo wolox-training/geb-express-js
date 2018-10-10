5d5b2db15f754c540eabb3fcf7a1737aa1af9746
bhflm <germanbonin@gmail.com>
2018-10-10 15:22:18 -0300

    few changes

---
+++ migrations/migrations/20181010184809-users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'role', Sequelize.STRING);
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'role', Sequelize.STRING);
+
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
