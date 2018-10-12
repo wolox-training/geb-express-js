b209998b2b6c0f5b93bf7aeb21663a707b947498
bhflm <germanbonin@gmail.com>
2018-10-12 19:32:41 -0300

    missing service albumManager

--- app/routes.js
+++ app/routes.js
@@ -6,5 +6,6 @@ exports.init = app => {
   app.post('/users', users.signUp);
   app.post('/users/sessions', users.logIn);
   app.get('/users', users.list);
+  app.get('/albums', users.albums);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
