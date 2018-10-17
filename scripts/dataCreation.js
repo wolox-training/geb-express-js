dcce0824e5091f0bb27255f6d3fb6d4bb805ce6e
bhflm <germanbonin@gmail.com>
2018-10-17 11:13:27 -0300

    travis

--- scripts/dataCreation.js
+++ scripts/dataCreation.js
@@ -6,57 +6,74 @@ exports.execute = () => {
   return bcrypt
     .hash('password28', 10)
     .then(hash => {
-      const data = [
+      const data = [];
+      data.push(
         albums.newEntry({
           ownedBy: 'juandoe@wolox.com.ar',
           album: 'quidem molestiae enim',
           albumId: '1'
-        }),
+        })
+      );
+      data.push(
         albums.newEntry({
           ownedBy: 'juandoe@wolox.com.ar',
           album: 'molestiae enim',
           albumId: '3'
-        }),
+        })
+      );
+      data.push(
         albums.newEntry({
           ownedBy: 'admin@wolox.com.ar',
           album: 'quidem molestiae enim',
           albumId: '1'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'admin',
           lastName: 'admin',
           email: 'admin@wolox.com.ar',
           password: hash,
           role: 'admin'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'john',
           lastName: 'doe',
           email: 'johndoe@wolox.com.ar',
           password: hash,
           role: 'user'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'john two',
           lastName: 'doe',
           email: 'johndoe2@wolox.com.ar',
           password: hash,
           role: 'user'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'john three',
           lastName: 'doe',
           email: 'johndoe3@wolox.com.ar',
           password: hash,
           role: 'user'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'john four',
           lastName: 'to the floor',
           email: 'johndoe4@wolox.com.ar',
           password: hash,
           role: 'user'
-        }),
+        })
+      );
+      data.push(
         users.newUser({
           firstName: 'juan',
           lastName: 'notdoe',
@@ -64,7 +81,7 @@ exports.execute = () => {
           password: hash,
           role: 'user'
         })
-      ];
+      );
       return Promise.all(data);
     })
     .catch(err => {
